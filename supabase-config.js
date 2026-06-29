464abf4271fa22224d70257439e735a8513c7efd
yeeun <yeeun7117@sookmyung.ac.kr>
2026-06-29 18:03:05 +0900

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.TWAIVE_SUPABASE = {
-  url: "YOUR_SUPABASE_PROJECT_URL",
-  anonKey: "YOUR_SUPABASE_ANON_KEY",
+  url: "https://bkwvndomtqhqveirgfgr.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrd3ZuZG9tdHFocXZlaXJnZmdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MTk0MjIsImV4cCI6MjA5ODI5NTQyMn0.Zt8RzFSEUhXXIfnLIDxQlTGA2Z1P_PZz7-KNHagpXKM",
 };
